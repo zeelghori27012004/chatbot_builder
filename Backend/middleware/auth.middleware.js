dfe0a5975dee117dd0d763cc9a6e91e8054c86f5
Zeel Ghori <ghorizeeljkvrz@gmail.com>
2026-02-05 11:27:49 +0530

    added middleware for authorization

---
+++ Backend/middleware/auth.middleware.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+import redisClient from "../services/redis.service.js";
+
+
+export const authUser = async (req, res, next) => {
+    try {
+        const token = req.cookies.token || req.headers.authorization.split(' ')[ 1 ];
+
+        if (!token) {
+            return res.status(401).send({ error: 'Unauthorized User' });
+        }
+
+        const isBlackListed = await redisClient.get(token);
+
+        if (isBlackListed) {
+
+            res.cookie('token', '');
+
+            return res.status(401).send({ error: 'Unauthorized User' });
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (error) {
+
+        console.log(error);
+
+        res.status(401).send({ error: 'Unauthorized User' });
+    }
+}
