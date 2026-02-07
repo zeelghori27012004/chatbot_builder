6be5ff9f5e397f1714593f54a42f5b9bf03d4f0a
Zeel Ghori <ghorizeeljkvrz@gmail.com>
2026-02-07 11:32:30 +0530

    added redis service

---
+++ Backend/services/redis.service.js
@@ -0,0 +1,15 @@
+import Redis from 'ioredis';
+
+
+const redisClient = new Redis({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    password: process.env.REDIS_PASSWORD
+});
+
+
+redisClient.on('connect', () => {
+    console.log('Redis connected');
+})
+
+export default redisClient;
